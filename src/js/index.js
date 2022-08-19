520e617dafafc1fbc46518b940c180ed95236861
khkim <kihyunkim@daou.co.kr>
2022-08-19 16:27:16 +0900

    품절 버튼 추가

--- src/js/index.js
+++ src/js/index.js
@@ -15,11 +15,11 @@ function App() {
   this.menu = [];
 
   this.init = () => {
+    this.menu = store.getStorage();
     render();
   };
 
   const render = () => {
-    this.menu = store.getStorage();
     const template = this.menu
       .map((item, index) => menuTemplate(item, index))
       .join("");
@@ -44,6 +44,38 @@ function App() {
     $(".menu-count").innerText = `총 ${count}개`;
   };
 
+  const editButton = (e) => {
+    let menuName = e.target.closest("li").querySelector(".menu-name");
+    const menuId = e.target.closest("li").dataset.menuId;
+    const updatedMenuName = prompt(
+      "메뉴 이름을 수정해 주세요",
+      menuName.innerText
+    );
+    if (updatedMenuName) {
+      menuName.innerText = updatedMenuName;
+      this.menu[menuId].name = updatedMenuName;
+      store.setStorage(this.menu);
+    }
+  };
+
+  const deleteButton = (e) => {
+    if (confirm("메뉴를 삭제하시겠습니까?")) {
+      const menuId = e.target.closest("li").dataset.menuId;
+      this.menu.splice(menuId, 1);
+      store.setStorage(this.menu);
+
+      e.target.closest("li").remove();
+      updateCount();
+    }
+  };
+
+  const toggleButton = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
+    this.menu[menuId].soldOut = !this.menu[menuId].soldOut;
+    store.setStorage(this.menu);
+    render();
+  };
+
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
@@ -62,26 +94,16 @@ function App() {
 
   $("#espresso-menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
-      let menuName = e.target.closest("li").querySelector(".menu-name");
-      const menuId = e.target.closest("li").dataset.menuId;
-      const updatedMenuName = prompt(
-        "메뉴 이름을 수정해 주세요",
-        menuName.innerText
-      );
-      menuName.innerText = updatedMenuName;
-      this.menu[menuId].name = updatedMenuName;
-      store.setStorage(this.menu);
+      editButton(e);
       return;
     }
     if (e.target.classList.contains("menu-remove-button")) {
-      if (confirm("메뉴를 삭제하시겠습니까?")) {
-        const menuId = e.target.closest("li").dataset.menuId;
-        this.menu.splice(menuId, 1);
-        store.setStorage(this.menu);
+      deleteButton(e);
+      return;
+    }
 
-        e.target.closest("li").remove();
-        updateCount();
-      }
+    if (e.target.classList.contains("menu-sold-out-button")) {
+      toggleButton(e);
       return;
     }
   });
--- src/template/menuTemplate.js
+++ src/template/menuTemplate.js
@@ -1,6 +1,13 @@
 const menuTemplate = (item, index) =>
   `<li data-menu-id=${index} class="menu-list-item d-flex items-center py-2">
-    <span class="w-100 pl-2 menu-name">${item.name}</span>
+    <span class="w-100 pl-2 menu-name 
+    ${item.soldOut ? "sold-out" : ""}" > ${item.name}</span>
+    <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+    >
+      품절
+    </button>
     <button
       type="button"
       class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
