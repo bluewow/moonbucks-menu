ce4432d4f480837a268a9e91069bc245521a616f
khkim <kihyunkim@daou.co.kr>
2022-08-19 13:37:13 +0900

    메뉴 수정

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,11 @@ const $ = (selector) => document.querySelector(selector);
 
 function App() {
   const render = () => {
+    if ($("#espresso-menu-name").value === "") {
+      alert("빈값입니다");
+      return;
+    }
+
     $("#espresso-menu-list").insertAdjacentHTML(
       "beforeend",
       menuTemplate({
@@ -27,6 +32,17 @@ function App() {
 
   $("#espresso-menu-submit-button").addEventListener("click", (e) => {
     render();
+  });
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      let menuName = e.target.closest("li").querySelector(".menu-name");
+      const updatedMenuName = prompt(
+        "메뉴 이름을 수정해 주세요",
+        menuName.innerText
+      );
+      menuName.innerText = updatedMenuName;
+    }
   });
 }
 
